b99a9f7989e6919f8654575d4fae97da01a19c1c
Alexandr Kurilin <alex@kurilin.net>
2014-03-14 01:19:51 -0700

    First commit

---
+++ Main.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Web.Scotty
+import Data.Monoid
+
+main = scotty 3000 $ do
+  get "/" $ do
+    text "gotten!"
+  get "/:num" $ do
+    (num :: Integer) <- param "word"
+    html $ mconcat ["<h1>Scotty, ", num, " me up!</h1>"]
+  delete "/" $ do
+    text "deleted!"
+  post "/" $ do
+    text "posted!"
+  put "/" $ do
+    text "put-ted!"
+  matchAny "/all" $ do
+    text "matches all methods"
+  notFound $ do
+    text "there is no such route."
