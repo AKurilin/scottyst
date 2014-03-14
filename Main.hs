3241330ebbb21ed150e13209b93a5e178f4f0493
Chris Allen <cma@bitemyapp.com>
2014-03-14 12:14:46 -0500

    yep

--- Main.hs
+++ Main.hs
@@ -1,13 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
 import Data.Monoid
+import qualified Data.Text.Lazy as T
+import Data.Text.Read
+import Control.Monad
 
 main = scotty 3000 $ do
   get "/" $ do
     text "gotten!"
   get "/:num" $ do
-    num <- param "num"
-    html $ mconcat ["<h1>Scotty, ", num, " me up!</h1>"]
+    num <- (liftM decimal) (param "num")
+    case num of
+      Left error -> html $ mconcat ["<h1>error yo: ", T.pack error]
+      Right (val, blah) -> html $ mconcat ["<h1>val + 1 == ", (T.pack . show) (val + 1), "</h1>"]
   delete "/" $ do
     text "deleted!"
   post "/" $ do
